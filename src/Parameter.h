9944982a5289a03f270ecdfc688709a9c32b95ea
dereklmc <derek.l.mclean@gmail.com>
2012-11-10 17:46:34 -0800

    Commented Parameter

--- src/Parameter.h
+++ src/Parameter.h
@@ -3,13 +3,24 @@
 
 #include "Variable.h"
 
+/**
+ * Represents an identifier for a parameter of a procedure or function.
+ * A parameter is a variable because it has a name and a type.
+ * A parameter can also be passed by reference or by value.
+ */
 class Parameter : public Variable {
 
     public:
+        // ctr, creates a parameter with a name and a type that is passed by ref
+        // or by value.
         Parameter(string, Type*, bool);
 
     private:
+        // records how the parameter is passed:
+        // true if by reference
+        // false if by value.
         bool byReference;
+        // Overloaded print from Symbol.
         void print(std::ostream&) const;
 
 };
--- src/parameter.cpp
+++ src/parameter.cpp
@@ -1,11 +1,24 @@
 #include"Parameter.h"
 
+/*******************************************************************************
+ *
+ * Constructor
+ *
+ * Creates a parameter with a name and a type that is either passed by reference
+ * or by value. Extends the constructor in Variable.
+ */
 Parameter::Parameter(string name, Type *type, bool byReference) :
         Variable(name, type)
 {
     this->byReference = byReference;
 }
 
+/*******************************************************************************
+ *
+ * Overloaded print from Variable.
+ * Extends Variable::print to also print whether the parameter is passed by
+ * reference or by value.
+ */
 void Parameter::print(std::ostream &output) const
 {
     if (byReference) {
