d141d34db93c37abc747d86c1e814093f00221f4
dereklmc <derek.l.mclean@gmail.com>
2012-11-10 19:40:56 -0800

    Implemented RecordType

---
+++ src/RecordType.h
@@ -0,0 +1,40 @@
+#ifndef RECORDTYPE_H
+#define RECORDTYPE_H
+
+#include "Type.h"
+#include "Variable.h"
+
+#include <iostream>
+#include <vector>
+
+/**
+ * Represents an identifier for type that is a record.
+ * A record has a name and a set of fields. Additionally, a record is assigned
+ * a pseudo-scope that is used for printing. The list of fields for a record may
+ * be empty.
+ */
+class RecordType : public Type {
+
+    public:
+        // ctr that creates a record with a name and a given pseudo-scope
+        RecordType(string, int);
+        // Add a new field to the current record.
+        bool addField(const Variable&);
+        // Checks if field with given name is in the current record.
+        bool hasField(const string&) const;
+        
+    protected:
+        // overriddent print method from Symbol. Prints name and fields.
+        void print(std::ostream&) const;
+
+    private:
+        // The set of fields within a record.
+        std::vector<Variable> fields;
+        // The pseudo-scope level used for printing purposes.
+        int scope;
+        // Helper method for #print, prints tabs indicated by scope.
+        void printScopeTabs(std::ostream&) const;
+
+};
+
+#endif
---
+++ src/recordtype.cpp
@@ -0,0 +1,74 @@
+#include "RecordType.h"
+
+/*******************************************************************************
+ *
+ * Standard constructor.
+ * Extends the constructor for Type to also take a parentScope. The parentScope
+ * is the id of the scope in which the current record is stored. The record then
+ * assigns itself a pseudo-scope within the parent scope. This pseudo-scope is
+ * used for printing fields.
+ */
+RecordType::RecordType(string name, int parentScope) :
+        Type(name)
+{
+    this->scope = parentScope + 1;
+}
+
+/*******************************************************************************
+ *
+ * Add a new field to the current record.
+ */
+bool RecordType::addField(const Variable &field)
+{
+    
+    if (hasField(field.name)) {
+        return false;
+    }
+    fields.push_back(field);
+    return true;
+}
+
+/*******************************************************************************
+ *
+ * Add a new field to the current record.
+ */
+bool RecordType::hasField(const String &fieldName)
+{
+    
+    for(it = fields.begin(); it != fields.end(); ++it) {
+        if (*it.name = field.name) {
+            return true;
+        }
+    }
+    false;
+}
+
+/*******************************************************************************
+ *
+ * Overloaded print method from Symbol.
+ *
+ * Prints name of the record and all fields contained in the record.
+ */
+void RecordType::print(std::ostream &output) const
+{
+    Type::print(output);
+    output << endl;
+    
+    std::vector<Variable>::const_iterator it;
+    for(it = fields.begin(); it != fields.end(); ++it) {
+        std::cout << *it << endl;
+    }
+}
+
+/*******************************************************************************
+ *
+ * Helper method for print.
+ * Prints to given output stream a number of tabs equal to the record's 
+ * psuedo-scope.
+ */
+void RecordType::printScopeTabs(std::ostream &output) const
+{    
+    for(int i = 0; i < scope; i++) {
+        std::cout << "\t";
+    }
+}
