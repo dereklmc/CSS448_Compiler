a6a5c6232d363936ff9ea6096c569d1a3fcf36b4
dereklmc <derek.l.mclean@gmail.com>
2012-11-10 17:15:07 -0800

    Added comments to Variable

--- src/Variable.h
+++ src/Variable.h
@@ -6,16 +6,24 @@
 
 #include <iostream>
 
+/**
+ * Represents an identifier for a variable and a field in a record.
+ * Has both a name and type.
+ */
 class Variable : public Symbol {
 
     public:
+        // ctr, creates a variable with a name and a type.
         Variable(string, Type*);
+        // Defined to release Variable's reference to Type.
+        // Since type is also in the symbol table, the ST will deallocate type.
         virtual ~Variable();
 
     private:
-        Type *type;
+        Type *type; // The type associated with this variable.
         
     protected:
+        // overriden print method from Symbol. Prints name and type's name.
         void print(std::ostream&) const;
 
 };
--- src/variable.cpp
+++ src/variable.cpp
@@ -1,16 +1,35 @@
-#include"Variable.h"
+#include "Variable.h"
 
+/*******************************************************************************
+ *
+ * Constructor
+ *
+ * Creates a variable with a name and a type. Extends the constructor in Symbol.
+ */
 Variable::Variable(string name, Type *type) :
         Symbol(name)
 {
     this->type = type;
 }
 
+/*******************************************************************************
+ *
+ * Destructor.
+ *
+ * Releases reference to type without deallocating type. Because the symbol
+ * table stores type, the symbol table is responsible for deallocating type.
+ */
 Variable::~Variable()
 {
     this->type = NULL;
 }
 
+/*******************************************************************************
+ *
+ * Overloaded print method from Symbol.
+ *
+ * Prints name of the variable and name of the variable's type.
+ */
 void Variable::print(std::ostream &output) const
 {
     Symbol::print(output);
