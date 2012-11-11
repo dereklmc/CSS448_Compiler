480435ef8363a5628efc4a6fda73de30b1fc51b8
dereklmc <derek.l.mclean@gmail.com>
2012-11-10 17:59:04 -0800

    Commented on type.

--- src/Type.h
+++ src/Type.h
@@ -3,9 +3,21 @@
 
 #include "symbol.h"
 
+/**
+ * Represents an identifier for an arbitrary type in pascal. This is an
+ * abstract base class for all types in pascal, including:
+ * * Records
+ * * Pointers
+ * * Arrays
+ * * Sets
+ *
+ * Currently, type is just a symbol with another name. Type may have specific
+ * methods in later phases of the compiler.
+ */
 class Type : public Symbol {
     
     public:
+        // Standard ctr, see Symbol.
         Type(string);
 };
 
--- src/type.cpp
+++ src/type.cpp
@@ -1,5 +1,11 @@
 #include "Type.h"
 
+/*******************************************************************************
+ *
+ * Standard constructor.
+ *
+ * See symbol for definition.
+ */
 Type::Type(string name) :
         Symbol(name)
 {
